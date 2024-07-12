0a010ff00b0bb9a81da907e8c2c34f68ea9695ba
Wiseephay99 <wiseephay99@gmail.com>
2024-07-12 10:52:21 +0300

    dictionaries and loops

---
+++ python/dictionaries.py
@@ -0,0 +1,14 @@
+print()
+print()
+print(f'===============DIctionaries==========')
+print(f'Dictionaries stores KEY - VALUE Pairs.')
+print()
+
+houses = {'James': 'Griffindor', 'Wise': 'Slitherin', "Samuel": 'Palatial'}
+
+print(houses)
+houses['Magdalene'] = 'Slitherin'
+print(houses['Magdalene'])
+
+for i in houses:
+    print(i)
---
+++ python/loops.py
@@ -0,0 +1,24 @@
+print()
+print()
+for i in [0, 1, 2, 3, 4, 5, 6]:
+    print(i)
+print()
+for i in range(100):
+    print( i, end=' ')
+    
+print()
+print()
+
+names = ["Harry", "Ron", "Wise", "Hermoine"]
+for name in names:
+    print(name)
+    
+print()
+name1 = 'EphayWise'
+for l in name1:
+    print(l)
+
+print()
+print()
+
+
